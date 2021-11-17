5e75b17803351f990be47056dcb7c2fcce06b0bc
Arthur Teixeira <52799942+arthurtc30@users.noreply.github.com>
2021-11-17 11:45:25 -0300

    Finalizando projeto
    Página de erro e rota finalizadas.

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Header from './components/Header';
 import Filme from './pages/Filme';
 import Favoritos from './pages/Favoritos';
+import Erro from './pages/Erro';
 
 const Rotas = () => {
     return (
@@ -12,6 +13,7 @@ const Rotas = () => {
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/filme/:id" element={<Filme />} />
                 <Route exact path="/favoritos" element={<Favoritos />} />
+                <Route path="*" element={<Erro />} />
             </Routes>
         </BrowserRouter>
     );
